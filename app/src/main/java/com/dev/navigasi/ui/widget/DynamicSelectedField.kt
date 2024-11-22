a95e7af3d05fb72008470792428b12c9a9a4f1ae
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-11-22 16:06:40 +0700

    Menambahkan fungsi komposabel DynamicSelectedTextField untuk membuat dropdown dinamis menggunakan Material3

--- app/src/main/java/com/dev/navigasi/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/dev/navigasi/ui/widget/DynamicSelectedField.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DynamicSelectedTextField(
