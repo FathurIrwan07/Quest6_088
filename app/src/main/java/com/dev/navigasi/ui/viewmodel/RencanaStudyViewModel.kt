bcff31d08d1c366e3b6c164f90380b444961b282
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-11-22 15:48:50 +0700

    Membuat kelas RencanaStudyViewModel yang merupakan turunan dari ViewModel.kelas ini mengelola state flow _krsState dengan tipe data RencanaStudy

--- app/src/main/java/com/dev/navigasi/ui/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/dev/navigasi/ui/viewmodel/RencanaStudyViewModel.kt
@@ -11,18 +11,5 @@ class RencanaStudyViewModel:ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudy())
     val krsStateUi: StateFlow<RencanaStudy> = _krsState.asStateFlow()
 
-    fun setMataKuliah(mkPilihan: String) {
-        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
-    }
 
-    fun setKelas(kelasPilihan: String){
-        _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
-    }
-
-    fun saveDataKRS(ls: MutableList<String>){
-        _krsState.update { status -> status.copy(
-            namaMK = ls[0],
-            kelas = ls [1]
-        ) }
-    }
 }
