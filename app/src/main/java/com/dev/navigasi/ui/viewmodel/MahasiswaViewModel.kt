1534253f25b341ae82526674653f16e236490261
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-11-22 15:35:31 +0700

    menambahkan class MahasiswaViewModel turunan dari class ViewModel dan menambahkan State Management dengan MutableStateFlow

--- app/src/main/java/com/dev/navigasi/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/dev/navigasi/ui/viewmodel/MahasiswaViewModel.kt
@@ -7,21 +7,10 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
+
 class MahasiswaViewModel: ViewModel() {
     private val _mahasiswaStateUI=
         MutableStateFlow(Mahasiswa())
 
-    val mahasiswaUiState:
-            StateFlow<Mahasiswa> =
-        _mahasiswaStateUI.asStateFlow()
 
-    fun  saveDataMahasiswa(ls: MutableList<String>){
-        _mahasiswaStateUI.update { statusSaatIni ->
-            statusSaatIni.copy(
-                nim = ls[0],
-                nama = ls[1],
-                email = ls[2],
-            )
-        }
-    }
 }
